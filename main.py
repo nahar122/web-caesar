b5e59bdc16ae8f575934c0a4b695399c0574ec8c
Your Name <you@example.com>
2018-09-18 17:59:19 -0400

    Encrypted text now shows within box

--- main.py
+++ main.py
@@ -9,42 +9,38 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
-            button {
-                background-color: #3366ff;
-                border-radius: 10px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <h1>Rotate by:</h1><input type="text" name="rot" value=0><br><br>
-            <textarea name="text"></textarea>
-            <input type="submit">
+            <textarea name="text">{0}</textarea>
+            <input class="submit" type="submit">
         </form>
     </body>
 </html>"""
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 @app.route("/",methods=['POST'])
 def encryption():
     rot = int(request.form["rot"])
     text = request.form["text"]
-    jumbled_test = rotate_string(text,rot)
-    return "<h1>" + jumbled_test + "</h1>"
+    encrypted_text = rotate_string(text,rot)
+    return form.format(encrypted_text)
     
 app.run()
